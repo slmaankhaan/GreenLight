d7de2bc1873a4638ddde8f53e14d9a1170b558a8
salmankhan <slmaan.khaan@outlook.com>
2025-01-07 13:23:17 -0500

    fixed-format JSON to send a well-formed JSON response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,8 +6,10 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintln(w, "environment: %s\n", app.config.env)
-	fmt.Fprintln(w, "version: %s\n", version)
+	js := `{"status", "available", "environment": %q, "version" : %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
 
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
