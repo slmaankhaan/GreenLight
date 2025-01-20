7a6be5813116d3d33a30a5557206d04300912fe7
salmankhan <slmaan.khaan@outlook.com>
2025-01-20 18:39:27 -0500

    Insert() method of database model and update this to create a new record in  movies table

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"github.com/lib/pq"
 	"github.com/slmaankhaan/GreenLight/internal/validator"
 	"time"
 )
@@ -37,7 +38,15 @@ type MovieModel struct {
 
 // Add a placeholder method for inserting a new record in the movies table.
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+
+	query := `
+			INSERT INTO movies (title, year, runtime, genres)
+			VALUES ($1, $2, $3, $4)
+			RETURNING id, created_at, version`
+
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 // Add a placeholder method for fetching a specific record from the movies table.
