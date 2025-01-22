5d57d9b8679cbb86adcd3be2f602475fa912317f
salmankhan <slmaan.khaan@outlook.com>
2025-01-22 13:34:43 -0500

    created a single global rate limiter for application

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/time/rate"
 	"net/http"
 )
 
@@ -17,6 +18,19 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// Initialize a new rate limiter which allows an average of 2 requests per second,
+	// with a maximum of 4 requests in a single ‘burst’.
+	limiter := rate.NewLimiter(2, 4)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/slmaankhaan/GreenLight
 go 1.23.4
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/lib/pq v1.10.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.0
+	golang.org/x/time v0.9.0
 )
