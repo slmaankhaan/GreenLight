7c206f202115aa8e5336e24266443fc1e7696dcb
salmankhan <slmaan.khaan@outlook.com>
2025-01-23 13:59:51 -0500

    Set up users database table and model

--- internal/data/models.go
+++ internal/data/models.go
@@ -12,10 +12,12 @@ var (
 
 type Models struct {
 	Movies MovieModel
+	Users  UserModel // Add a new Users field.
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db}, // Initialize a new UserModel instance.
 	}
 }
